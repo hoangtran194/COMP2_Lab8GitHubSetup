908c13bb6b8d30cccde4f212edbbce831f60d43b
Huy Hoang Tran <htran116@my.centennialcollege.ca>
2019-06-26 22:34:32 -0400

    Project Initialize.

---
+++ Lab2/Lab08Form.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Lab2
+{
+    partial class Lab08Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.infoGroupBox = new System.Windows.Forms.GroupBox();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.infoGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(13, 13);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(172, 61);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Lab08";
+            // 
+            // infoGroupBox
+            // 
+            this.infoGroupBox.BackColor = System.Drawing.Color.Gray;
+            this.infoGroupBox.Controls.Add(this.ageTextBox);
+            this.infoGroupBox.Controls.Add(this.ageLabel);
+            this.infoGroupBox.Controls.Add(this.nameTextBox);
+            this.infoGroupBox.Controls.Add(this.nameLabel);
+            this.infoGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoGroupBox.Location = new System.Drawing.Point(12, 86);
+            this.infoGroupBox.Name = "infoGroupBox";
+            this.infoGroupBox.Size = new System.Drawing.Size(590, 200);
+            this.infoGroupBox.TabIndex = 1;
+            this.infoGroupBox.TabStop = false;
+            this.infoGroupBox.Text = "info";
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ageTextBox.Location = new System.Drawing.Point(355, 113);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(229, 68);
+            this.ageTextBox.TabIndex = 2;
+            this.ageTextBox.TextChanged += new System.EventHandler(this.ageTextBox_TextChanged);
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ageLabel.Location = new System.Drawing.Point(57, 112);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(300, 63);
+            this.ageLabel.TabIndex = 2;
+            this.ageLabel.Text = "Enter age: ";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTextBox.Location = new System.Drawing.Point(355, 33);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(229, 68);
+            this.nameTextBox.TabIndex = 1;
+            this.nameTextBox.TextChanged += new System.EventHandler(this.nameTextBox_TextChanged);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(12, 38);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(346, 63);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Enter name: ";
+            // 
+            // submitButton
+            // 
+            this.submitButton.BackColor = System.Drawing.Color.OrangeRed;
+            this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(379, 317);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(217, 80);
+            this.submitButton.TabIndex = 3;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = false;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(12, 327);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(361, 63);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "Enter age: ";
+            // 
+            // Lab08Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(614, 409);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.infoGroupBox);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Lab08Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab08";
+            this.Load += new System.EventHandler(this.Lab08Form_Load);
+            this.infoGroupBox.ResumeLayout(false);
+            this.infoGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.GroupBox infoGroupBox;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Lab2/Lab08Form.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class Lab08Form : Form
+    {
+        public string name;
+        public float age;
+
+        public Lab08Form()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            name = nameTextBox.Text;
+            //option A
+            age = Convert.ToSingle(ageTextBox.Text);
+
+            //option B
+            age = float.Parse(ageTextBox.Text);
+
+            //option C
+            float tempFloat;
+            bool result = float.TryParse(ageTextBox.Text, out tempFloat);
+            age = tempFloat;
+
+            //option D
+            //age = (float)ageTextBox.Text;
+
+            clearTextBox();
+        }
+
+        private void clearTextBox()
+        {
+            nameTextBox.Clear();
+            ageTextBox.Clear();
+        }
+
+        private void Lab08Form_Load(object sender, EventArgs e)
+        {
+            submitButton.Enabled = false;
+        }
+
+        private void nameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            validateTextbox();
+        }
+        
+        private void ageTextBox_TextChanged(object sender, EventArgs e)
+        {
+            validateTextbox();
+        }
+
+        private void validateTextbox()
+        {
+            bool condition1, condition2;
+            condition1 = condition2 = false;
+
+            try
+            {
+                age = float.Parse(ageTextBox.Text);
+                condition1 = true;
+            }
+            catch
+            {
+                submitButton.Enabled = false;
+                condition1 = false;
+            }
+            condition2 = (nameTextBox.Text.Length >= 2) ? true : false;
+
+            if (condition2 == true && condition1 == true)
+            {
+                submitButton.Enabled = true;
+            }
+            else
+            {
+                submitButton.Enabled = false;
+            }
+        }
+
+    }
+}
